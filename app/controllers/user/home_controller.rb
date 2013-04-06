734701f79a1e19d716123f0ecf5aac987b3dad42
Renato Zust <renatozust@gmail.com>
2013-04-06 13:05:29 -0500

    changes

--- app/controllers/user/home_controller.rb
+++ app/controllers/user/home_controller.rb
@@ -1,6 +1,8 @@
 class User::HomeController < ApplicationController
-  before_filter :authenticate_user!
-	def index
+  before_filter :authenticate_user!, :except => [:index, :pricing, :how_it_works, :contact_us, :help]
+	
+  def index 
+    flash[:alert] = "(Prototipo)"
 		render :index
 	end
 
@@ -23,4 +25,21 @@ class User::HomeController < ApplicationController
   def devices
     flash[:alert] = "(Prototipo)"
   end
+
+  def pricing
+    flash[:alert] = "(Prototipo)"
+  end
+
+  def how_it_works
+    flash[:alert] = "(Prototipo)"
+  end
+
+  def contact_us
+    flash[:alert] = "(Prototipo)"
+  end
+
+  def help
+    flash[:alert] = "(Prototipo)"
+  end
+
 end
