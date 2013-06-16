02fd1f4d8dc43cc6ae21d958a5c808c5bb48a9f3
Renato Zust <renatozust@gmail.com>
2013-06-16 00:40:01 -0500

    payment update

--- app/controllers/user/subscriptions_controller.rb
+++ app/controllers/user/subscriptions_controller.rb
@@ -1,17 +1,27 @@
 class User::SubscriptionsController < ApplicationController
+
+  def new
+    @subscription_type = SubscriptionType.find_by_id(params[:subscription_type_id])
+  end
+
   def create
-    amount = 19900
-    charge = Stripe::Charge.create(
-            :amount => amount,
-            :currency => "usd",
-            :card => params["stripeToken"], # obtained with Stripe.js
-            :description => "PRAIG Subscription"
-          )
-    if charge.paid
-      ip = request.env['REMOTE_ADDR']
-      subscription = Subscription.create(:user_id => current_user.id, :start_date => Time.now.to_date, :end_date => Time.now.to_date+1.year)
-      Payment.create(:subscription_id => subscription.id, :amount => amount.to_f / 100, :card_type => charge.card.type, :last4 => charge.card.last4)
-      flash[:success] = "Subscription Successful!"
+    subscription_type = SubscriptionType.find_by_id(params[:subscription])
+    unless subscription_type.nil? 
+      amount = (subscription_type.price * 100).to_i
+      charge = Stripe::Charge.create(
+              :amount => amount,
+              :currency => "usd",
+              :card => params["stripeToken"], # obtained with Stripe.js
+              :description => "PRAIG Subscription"
+            )
+      if charge.paid
+        ip = request.env['REMOTE_ADDR']
+        subscription = Subscription.create(:user_id => current_user.id, :start_date => Time.now.to_date, :end_date => Time.now.to_date+1.year)
+        Payment.create(:subscription_id => subscription.id, :amount => amount.to_f / 100, :card_type => charge.card.type, :last4 => charge.card.last4)
+        flash[:success] = "Subscription Successful!"
+      else
+        flash[:error] = "Payment Unsuccessful"
+      end
     else
       flash[:error] = "Payment Unsuccessful"
     end
